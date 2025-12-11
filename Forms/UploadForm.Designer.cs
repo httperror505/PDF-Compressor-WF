6edb74cb50bbb0bc16f10dffb5918358b4e50893
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-12-12 04:15:35 +0800

    Refine UploadForm layout and control properties
    Adjusted positions, sizes, margins, and fonts of controls for improved alignment and appearance. Added Anchor properties to controls for better layout behavior. Updated form dimensions and scaling for a more consistent UI.

--- Forms/UploadForm.Designer.cs
+++ Forms/UploadForm.Designer.cs
@@ -39,10 +39,13 @@
             // 
             // button1
             // 
+            button1.Anchor = AnchorStyles.None;
             button1.BackColor = SystemColors.GradientActiveCaption;
-            button1.Location = new Point(542, 178);
+            button1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(550, 222);
+            button1.Margin = new Padding(0);
             button1.Name = "button1";
-            button1.Size = new Size(132, 46);
+            button1.Size = new Size(121, 36);
             button1.TabIndex = 0;
             button1.Text = "Upload File";
             button1.UseVisualStyleBackColor = false;
@@ -50,24 +53,28 @@
             // 
             // textBox1
             // 
+            textBox1.Anchor = AnchorStyles.None;
             textBox1.BackColor = SystemColors.GradientActiveCaption;
-            textBox1.Location = new Point(110, 178);
+            textBox1.Location = new Point(177, 222);
+            textBox1.Margin = new Padding(0);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(426, 46);
+            textBox1.Size = new Size(373, 36);
             textBox1.TabIndex = 1;
             // 
             // button2
             // 
+            button2.Anchor = AnchorStyles.None;
             button2.BackColor = SystemColors.Highlight;
             button2.BackgroundImageLayout = ImageLayout.None;
             button2.FlatAppearance.BorderSize = 0;
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button2.ForeColor = SystemColors.ButtonHighlight;
-            button2.Location = new Point(302, 267);
+            button2.Location = new Point(345, 288);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(170, 41);
+            button2.Size = new Size(149, 31);
             button2.TabIndex = 2;
             button2.Text = "Compress File";
             button2.UseVisualStyleBackColor = false;
@@ -75,51 +82,56 @@
             // 
             // progressBar1
             // 
+            progressBar1.Anchor = AnchorStyles.None;
             progressBar1.BackColor = Color.Chartreuse;
             progressBar1.ForeColor = Color.Chartreuse;
-            progressBar1.Location = new Point(160, 239);
+            progressBar1.Location = new Point(221, 267);
+            progressBar1.Margin = new Padding(3, 2, 3, 2);
             progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(467, 10);
+            progressBar1.Size = new Size(409, 8);
             progressBar1.TabIndex = 3;
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new Font("Open Sans ExtraBold", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(222, 65);
+            label1.Location = new Point(273, 133);
             label1.Name = "label1";
-            label1.Size = new Size(376, 58);
+            label1.Size = new Size(300, 46);
             label1.TabIndex = 4;
             label1.Text = "File Compresssor";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.None;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ControlDarkDark;
-            label2.Location = new Point(211, 137);
+            label2.Location = new Point(266, 191);
             label2.Name = "label2";
-            label2.Size = new Size(392, 20);
+            label2.Size = new Size(307, 15);
             label2.TabIndex = 5;
             label2.Text = "Compressing your file without compromising quality.";
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.ControlDarkDark;
-            label3.Location = new Point(398, 424);
+            label3.Location = new Point(523, 493);
             label3.Name = "label3";
-            label3.Size = new Size(390, 17);
+            label3.Size = new Size(328, 13);
             label3.TabIndex = 6;
             label3.Text = "Product by zone7systems. Unauthorized sharing is prohibited.";
             // 
             // UploadForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(863, 515);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -127,6 +139,7 @@
             Controls.Add(button2);
             Controls.Add(textBox1);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "UploadForm";
             Text = "File Compressor";
             ResumeLayout(false);
